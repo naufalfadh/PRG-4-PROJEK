08a18508bad8f5a1cf6c98da050d8ff37e7d261f
Ffathiyl <m.fatihalfalahi2003@gmail.com>
2024-01-29 14:13:54 +0700

    done

--- PRG-4-PROJEK/Program.cs
+++ PRG-4-PROJEK/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddSession(options =>
 {
-    options.IdleTimeout = TimeSpan.FromMinutes(5);
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
 });
 var app = builder.Build();
 
